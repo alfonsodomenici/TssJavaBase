f15643632e5e449d80ebc81c61f9e7c18f4f42ad
tss <tss@debian-tss24>
2016-11-07 16:04:11 +0100

    aggiunto unput frase utente

--- src/lezione1/Main.java
+++ src/lezione1/Main.java
@@ -5,6 +5,8 @@
  */
 package lezione1;
 
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author tss
@@ -13,5 +15,7 @@ public class Main {
     
     public static void main(String[] args) {
         System.out.println("primo software started....");
+        String frase = JOptionPane.showInputDialog("Inserisci la frase da stampare");
+        System.out.println(frase);
     }
 }

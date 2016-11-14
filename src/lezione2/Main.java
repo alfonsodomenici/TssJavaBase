2baf721d53b28630438d7289642aeefa24aee4b3
tss <tss@debian-tss24>
2016-11-14 12:32:47 +0100

    creato packahe lezione2

--- src/lezione2/Main.java
+++ src/lezione2/Main.java
@@ -11,20 +11,49 @@ package lezione2;
  * @author tss
  */
 public class Main {
-    
+
+    //variabile statica 
+    private static int colore;
+
+    //variabile di istanza
+    private int carattere;
+
     /**
      * Da questo metodo partirà l'esecuzione del programma
-     * 
-     * @param args 
+     *
+     * @param args
      */
     public static void main(String[] args) {
         // singola riga
-        
+
         /*
         commento su più righe
         ecc...
-        */
-        
+         */
+        System.out.println("Parametri in ingrasso: " + args);
+
         System.out.println("Start esempio commenti..");
+
+        //variabile locale
+        int totale = 10;
+        String frase;
+
+        totale -= 20;
+
+        if (totale > 0) {
+            //variabile locale
+            int x = 5;
+            frase = "Il totale è positivo";
+        } else {
+            int x = 10;
+            frase = "Il totale è negativo";
+        }
+
+        //if ternario 
+        frase = (totale > 0) ? "Il totale è positivo" : "Il totale è negativo";
+
+        System.out.println("Totale: " + totale);
+        System.out.println(frase);
+
     }
 }
